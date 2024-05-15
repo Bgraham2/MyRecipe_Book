3cfb83991d4dc1b5badea66cb31383fe4e8bbadd
BrianGraham <bgraham2@gmail.com>
2024-05-15 09:53:17 -0500

    Added recipe type

---
+++ app/src/main/java/com/example/myrecipebook/RecipeType.java
@@ -0,0 +1,18 @@
+package com.example.myrecipebook;
+
+public class RecipeType {
+
+    private String type;
+
+    public RecipeType(String type) {
+        this.type = type;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
